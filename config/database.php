ed5f2137f21660d24c55cf893b47fad5eed8b38c
QQAnh <31837158+QQAnh@users.noreply.github.com>
2018-06-13 16:12:16 +0700

    Added DB Config

--- config/database.php
+++ config/database.php
@@ -41,11 +41,11 @@ return [
 
         'mysql' => [
             'driver' => 'mysql',
-            'host' => env('DB_HOST', '127.0.0.1'),
+            'host' => env('DB_HOST', 'us-cdbr-iron-east-04.cleardb.net'),
             'port' => env('DB_PORT', '3306'),
-            'database' => env('DB_DATABASE', 'forge'),
-            'username' => env('DB_USERNAME', 'forge'),
-            'password' => env('DB_PASSWORD', ''),
+            'database' => env('DB_DATABASE', 'heroku_f6a225dc47d8fa9'),
+            'username' => env('DB_USERNAME', 'beddd0a8da8c14'),
+            'password' => env('DB_PASSWORD', '15aa1af4'),
             'unix_socket' => env('DB_SOCKET', ''),
             'charset' => 'utf8mb4',
             'collation' => 'utf8mb4_unicode_ci',
