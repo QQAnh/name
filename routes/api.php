69cbe22c6f6b0e9056ad590e33578270ebd91640
QQAnh <31837158+QQAnh@users.noreply.github.com>
2018-06-13 11:50:10 +0700

    new

--- routes/api.php
+++ routes/api.php
@@ -24,41 +24,36 @@ Route::group(['middleware'=>'auth:api'],function (){
 
 
 
+Route::get('/user', function(Request $request){
+   $entries =
+        [[
+            'Fullname' => 'Hoàng Quynh Anh',
+            'Date_of_Birth' => '01-07-1999',
+            'Email'=>'hoangquynhanh@gmail.com',
+            'Phone'=>'012321313',
+            'Address'=>'Ha noi ',
 
-//        [[
-//            'Fullname' => 'Hoàng Quynh Anh',
-//            'Date_of_Birth' => '01-07-1999',
-//            'Email'=>'hoangquynhanh@gmail.com',
-//            'Phone'=>'012321313',
-//            'Address'=>'Ha noi ',
-//
-//        ],[
-//            'Fullname' => 'Nguyen Van Loc ',
-//            'Date_of_Birth' => '02-07-1999',
-//            'Email'=>'nguyenvanloc@gmail.com',
-//            'Phone'=>'1231311313',
-//            'Address'=>'Ninh Binh',
-//
-//        ],[
-//            'Fullname' => 'Tran Binh Minh',
-//            'Date_of_Birth' => '03-04-1995',
-//            'Email'=>'tranbinhminh@gmail.com',
-//            'Phone'=>'0123123123',
-//            'Address'=>'Nghe An',
-//
-//        ],[
-//            'Fullname' => 'Vu Tran Hoang',
-//            'Date_of_Birth' => '11-12-1992',
-//            'Email'=>'vutranhoang@gmail.com',
-//            'Phone'=>'123123132',
-//            'Address'=>'Ha Tinh',
-//        ],
-//    ];
-Route::get('/user', function (Request $request) {
-    $entries = \App\User::all();
-    return response()->json($entries, 200);
-});
-Route::get('/user/{id}', function (Request $request , $id) {
-    $entries = \App\User::find($id);
+        ],[
+            'Fullname' => 'Nguyen Van Loc ',
+            'Date_of_Birth' => '02-07-1999',
+            'Email'=>'nguyenvanloc@gmail.com',
+            'Phone'=>'1231311313',
+            'Address'=>'Ninh Binh',
+
+        ],[
+            'Fullname' => 'Tran Binh Minh',
+            'Date_of_Birth' => '03-04-1995',
+            'Email'=>'tranbinhminh@gmail.com',
+            'Phone'=>'0123123123',
+            'Address'=>'Nghe An',
+
+        ],[
+            'Fullname' => 'Vu Tran Hoang',
+            'Date_of_Birth' => '11-12-1992',
+            'Email'=>'vutranhoang@gmail.com',
+            'Phone'=>'123123132',
+            'Address'=>'Ha Tinh',
+        ],
+    ];
     return response()->json($entries, 200);
 });
