0070ba30c7942ff9d9a8d48c7cb43bcbc1a692ea
QQAnh <31837158+QQAnh@users.noreply.github.com>
2018-07-18 14:25:00 +0700

    Update api

--- app/Http/Controllers/Client/loginController.php
+++ app/Http/Controllers/Client/loginController.php
@@ -22,7 +22,7 @@ class loginController extends Controller
     }
     public function handleRequest(Request $request){
 
-        return $request->input('name');
+        return $request->input('inputname');
 
     }
 
