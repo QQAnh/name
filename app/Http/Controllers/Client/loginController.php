9e63d05ef7878ece62c5b5118d6b54fb9d3604b8
QQAnh <31837158+QQAnh@users.noreply.github.com>
2018-07-18 20:09:40 +0700

    Update api

--- app/Http/Controllers/Client/loginController.php
+++ app/Http/Controllers/Client/loginController.php
@@ -34,7 +34,7 @@ class loginController extends Controller
 
         $phone = $request->input('phone');
 //        $password = $request->input('password');
-        $account =Account::where('phone',$phone)->get();
+        $account =Account::where('phone',$phone)->value('password')->get();
 
         return $account;
 
