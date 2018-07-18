e45df0ba6efc331d7abca0480d55fe48f1a6a5e3
QQAnh <31837158+QQAnh@users.noreply.github.com>
2018-07-18 12:19:12 +0700

    Update api

--- app/Http/Controllers/Client/loginController.php
+++ app/Http/Controllers/Client/loginController.php
@@ -7,40 +7,12 @@ use App\Http\Controllers\Controller;
 
 class loginController extends Controller
 {
-    public function phone()
+    public function getLogin()
     {
-        return 'phone';
-    }
-    public function password(){
-        return 'password';
+        return view('client.listClient.login');
+
     }
 
 
-    public function postLogin(Request $request)
-    {
-        $rules = [
-            'phone' => 'required|phone',
-            'password' => 'required'
-        ];
-        $messages = [
-            'phone.required' => 'Email là trường bắt buộc',
-            'password.required' => 'Mật khẩu là trường bắt buộc',
-            'password.min' => 'Mật khẩu phải chứa ít nhất 8 ký tự',
-        ];
-        $validator = Validator::make($request->all(), $rules, $messages);
 
-        if ($validator->fails()) {
-            return redirect()->back()->withErrors($validator)->withInput();
-        } else {
-            $email = $request->input('phone');
-            $password = $request->input('password');
-
-            if (Auth::attempt(['phone' => $email, 'password' => $password])) {
-                return redirect()->intended('/');
-            } else {
-                $errors = new MessageBag(['errorlogin' => 'Phone hoặc mật khẩu không đúng']);
-                return redirect()->back()->withInput()->withErrors($errors);
-            }
-        }
-    }
 }
--- routes/web.php
+++ routes/web.php
@@ -20,8 +20,7 @@ Auth::routes();
 Route::get('/homepage','Client\HomepageController@index');
 Route::get('/test/{id}','Client\HomepageController@test');
 Route::get('/register','Client\HomepageController@register');
-Route::get('/login','Client\HomepageController@getLogin');
-Route::post('/logintest','Client\testController@check');
+Route::get('/login','Client\loginController@getLogin');
 
 
 
