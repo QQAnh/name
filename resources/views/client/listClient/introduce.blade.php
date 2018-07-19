a4e03b8a469b1a8970f107522d327cb7af113bae
QQAnh <31837158+QQAnh@users.noreply.github.com>
2018-07-19 09:58:37 +0700

    Update api

--- resources/views/client/listClient/introduce.blade.php
+++ resources/views/client/listClient/introduce.blade.php
@@ -2,14 +2,16 @@
 @section('title', 'Giới Thiệu')
 
 <style>
-    .breadcrumb.brand-success{
-        background-color:#5cb85c;
+    .breadcrumb.brand-success {
+        background-color: #5cb85c;
     }
-    .breadcrumb.brand-success a{
-        color:#fff;
+
+    .breadcrumb.brand-success a {
+        color: #fff;
     }
-    .breadcrumb.brand-success li.active{
-        color:#eee;
+
+    .breadcrumb.brand-success li.active {
+        color: #eee;
     }
 </style>
 @section('content')
@@ -18,71 +20,77 @@
         <div class="container">
             <div class="row">
                 <div class="col-md-3">
-
-                    <div class="menu-about">
-                        <ol class="breadcrumb brand-success">
-                            <li><a href="#">Home</a></li>
-                            <li><a href="#">Library</a></li>
-                            <li class="active">Data</li>
-                        </ol>
-                    </div>
                 </div>
                 <div class="col-md-9">
+                    <ol class="breadcrumb brand-success">
+                        <li><a href="#">Home</a></li>
+                        <li><a href="#">Library</a></li>
+                        <li class="active">Data</li>
+                    </ol>
 
-                    <div class="breadcrumb clearfix">
-                        <ul>
-                            <li itemtype="http://data-vocabulary.org/Breadcrumb" itemscope="" class="home">
-                                <a title="Đến trang chủ" href="/" itemprop="url"><span itemprop="title">Trang chủ</span></a>
-                            </li>
-                            <li itemtype="http://data-vocabulary.org/Breadcrumb" itemscope="" class="icon-li">
-                                <a title="Giới thiệu" href="/gioi-thieu.html" itemprop="url"><span itemprop="title">Giới thiệu</span></a>
-                            </li>
-                            <li class="icon-li"><strong>Về ch&#250;ng t&#244;i</strong> </li>
-                        </ul>
-                    </div>
-                    <script type="text/javascript">
-                        $(".link-site-more").hover(function () { $(this).find(".s-c-n").show(); }, function () { $(this).find(".s-c-n").hide(); });
-                    </script>
-                    <div class="about-detail">
-                        <div class="about-block">
-                            <h1>Về ch&#250;ng t&#244;i</h1>
-                            <div class="content">
-                                <div>
-                                    Công ty Cổ phần Tư vấn công nghệ, thiết bị và Kiểm định xây dựng - CONINCO là một doanh nghiệp tư vấn đầu tư và xây dựng, hoạt động đa lĩnh vực, đa ngành nghề trong phạm vi cả nước, hợp tác kinh doanh hiệu quả với nhiều Công ty và tập đoàn kinh tế lớn trên thế giới.</div>
-                                <div>
-                                </div>
-                                <div>
-                                    Với triết lý " Chắc chắn cho tương lai", chúng tôi luôn hướng tới mục tiêu phát triển bền vững cho chính Công ty và cho khách hàng của mình.</div>
-                                <div>
-                                </div>
-                                <div>
-                                    Từ ngày thành lập 16 tháng 4 năm 1979 đến nay, trải qua hơn 30 năm xây dựng và trưởng thành, tập thể lãnh đạo, cán bộ công nhân viên Công ty đã nỗ lực không ngừng xây dựng và phát triển. CONINCO đã được Đảng và Nhà nước ghi nhận thành tích và tặng thưởng nhiều danh hiệu cao quý, góp phần tạo dựng thương hiệu CONINCO là một trong những Công ty hàng đầu trong Tập đoàn Tư vấn Xây dựng Việt Nam.</div>
-                                <div>
-                                </div>
-                                <div>
-                                    CONINCO cũng được các Chủ đầu tư và khách hàng đánh giá là một thương hiệu Tư vấn Xây dựng lớn có uy tín, tin cậy, có thể độc lập đảm đương thực hiện công tác tư vấn cho các dự án quy mô lớn, phức tạp về công nghệ, kỹ thuật.</div>
-                                <div>
-                                </div>
-                                <div>
-                                    Hiện nay, với mô hình hoạt động mới là một Công ty đại chúng, sau khi thực hiện thành công và hiệu quả mô hình Nhượng quyền thương mại cho các Công ty thành viên, cùng với sự quan tâm, giúp đỡ và chỉ đạo kịp thời của Lãnh đạo các cấp, các ngành, sự tin cậy của Quý khách hàng, thương hiệu CONINCO ngày càng phát triển và lớn mạnh.</div>
-                                <div>
-                                </div>
-                                <div>
-                                    Bằng việc kiên trì thực hiện Hệ thống quản lý chất lượng phù hợp với tiêu chuẩn Quốc tế ISO 9001: 2008, CONINCO đảm bảo cung cấp các dịch vụ và sản phẩm tốt nhất để làm hài lòng Quý vị khách hàng, hy vọng mang lại được nhiều lợi ích cho cộng đồng góp phần vào sự nghiệp xây dựng phát triển đất nước.</div>
+                </div>
+                <script type="text/javascript">
+                    $(".link-site-more").hover(function () {
+                        $(this).find(".s-c-n").show();
+                    }, function () {
+                        $(this).find(".s-c-n").hide();
+                    });
+                </script>
+            </div>
 
+                <div class="about-detail">
+                    <div class="about-block">
+                        <h1>Về ch&#250;ng t&#244;i</h1>
+                        <div class="content">
+                            <div>
+                                Công ty Cổ phần Tư vấn công nghệ, thiết bị và Kiểm định xây dựng - CONINCO là một doanh
+                                nghiệp tư vấn đầu tư và xây dựng, hoạt động đa lĩnh vực, đa ngành nghề trong phạm vi cả
+                                nước, hợp tác kinh doanh hiệu quả với nhiều Công ty và tập đoàn kinh tế lớn trên thế
+                                giới.
                             </div>
+
+                            <div>
+                                Với triết lý " Chắc chắn cho tương lai", chúng tôi luôn hướng tới mục tiêu phát triển
+                                bền vững cho chính Công ty và cho khách hàng của mình.
+                            </div>
+                            <div>
+                            </div>
+                            <div>
+                                Từ ngày thành lập 16 tháng 4 năm 1979 đến nay, trải qua hơn 30 năm xây dựng và trưởng
+                                thành, tập thể lãnh đạo, cán bộ công nhân viên Công ty đã nỗ lực không ngừng xây dựng và
+                                phát triển. CONINCO đã được Đảng và Nhà nước ghi nhận thành tích và tặng thưởng nhiều
+                                danh hiệu cao quý, góp phần tạo dựng thương hiệu CONINCO là một trong những Công ty hàng
+                                đầu trong Tập đoàn Tư vấn Xây dựng Việt Nam.
+                            </div>
+                            <div>
+                            </div>
+                            <div>
+                                CONINCO cũng được các Chủ đầu tư và khách hàng đánh giá là một thương hiệu Tư vấn Xây
+                                dựng lớn có uy tín, tin cậy, có thể độc lập đảm đương thực hiện công tác tư vấn cho các
+                                dự án quy mô lớn, phức tạp về công nghệ, kỹ thuật.
+                            </div>
+                            <div>
+                            </div>
+                            <div>
+                                Hiện nay, với mô hình hoạt động mới là một Công ty đại chúng, sau khi thực hiện thành
+                                công và hiệu quả mô hình Nhượng quyền thương mại cho các Công ty thành viên, cùng với sự
+                                quan tâm, giúp đỡ và chỉ đạo kịp thời của Lãnh đạo các cấp, các ngành, sự tin cậy của
+                                Quý khách hàng, thương hiệu CONINCO ngày càng phát triển và lớn mạnh.
+                            </div>
+                            <div>
+                            </div>
+                            <div>
+                                Bằng việc kiên trì thực hiện Hệ thống quản lý chất lượng phù hợp với tiêu chuẩn Quốc tế
+                                ISO 9001: 2008, CONINCO đảm bảo cung cấp các dịch vụ và sản phẩm tốt nhất để làm hài
+                                lòng Quý vị khách hàng, hy vọng mang lại được nhiều lợi ích cho cộng đồng góp phần vào
+                                sự nghiệp xây dựng phát triển đất nước.
+                            </div>
+
                         </div>
                     </div>
                 </div>
             </div>
         </div>
-    </div>
 
-                        </div>
-                    </div>
-
-            </div>
-        </div>
-    </div>
 
 @endsection
