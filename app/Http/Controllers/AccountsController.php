3c60978a6d0eade30294b76840a0455d1054f2be
QQAnh <31837158+QQAnh@users.noreply.github.com>
2018-07-19 15:41:41 +0700

    Update api

--- app/Http/Controllers/AccountsController.php
+++ app/Http/Controllers/AccountsController.php
@@ -99,22 +99,24 @@ class AccountsController extends Controller
             return view("errors.404");
         }
         $user->salt = rand();
+        $password = $request->input('password');
+        return md5($password);
 
-        $user->fullname = $request->get("fullname");
-        $user->phone = $request->get("phone");
-        $user->password = md5($request->get('password'));
-        $user->gender = $request->get("gender");
-        $user->email = $request->get("email");
-        $user->role = 'Member';
-
-        $user->status = 1;
-        $user->avatar = $request->get('avatar2');
-        $user->save();
-        if ($request->get("isAjax")) {
-            return $user;
-        } else {
-            return redirect("user");
-        }
+//        $user->fullname = $request->get("fullname");
+//        $user->phone = $request->get("phone");
+//        $user->password = $request->get('password');
+//        $user->gender = $request->get("gender");
+//        $user->email = $request->get("email");
+//        $user->role = 'Member';
+//
+//        $user->status = 1;
+//        $user->avatar = $request->get('avatar2');
+//        $user->save();
+//        if ($request->get("isAjax")) {
+//            return $user;
+//        } else {
+//            return redirect("user");
+//        }
     }
 
     /**
