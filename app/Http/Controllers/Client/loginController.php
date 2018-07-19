e77f8da505c6c0f479eaa7e78843cb894345afd9
QQAnh <31837158+QQAnh@users.noreply.github.com>
2018-07-19 14:48:38 +0700

    Update api

--- app/Http/Controllers/Client/loginController.php
+++ app/Http/Controllers/Client/loginController.php
@@ -46,7 +46,7 @@ class loginController extends Controller
             return view('admin/listAdmin/dashboard');
         }else{
 
-            return view('client.listClient.login')->flash('successMsg','Saved succesfully!');
+            return view('client.listClient.login');
         }
 //        return md5($password.$salt);
 //        $account = DB::table('accounts')->where('phone',$phone)->get();
