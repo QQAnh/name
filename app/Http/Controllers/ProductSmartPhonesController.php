8e512118e3e701fabf30110656cc5ea82ca80f5b
QQAnh <31837158+QQAnh@users.noreply.github.com>
2018-07-23 09:05:04 +0700

    Update api

--- app/Http/Controllers/ProductSmartPhonesController.php
+++ app/Http/Controllers/ProductSmartPhonesController.php
@@ -31,7 +31,7 @@ class ProductSmartPhonesController extends Controller
     {
         return view('admin.listAdmin.Product.SmartPhone.formProductPhone')->with([
             "product"=> new Product(),
-            "action"=>"/smartphone",
+            "action"=>"/admin/smartphone",
             "method"=>"POST"
         ]);
     }
