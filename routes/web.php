d988e8f2b284a004faf88ad5748a1f292aadcb32
QQAnh <31837158+QQAnh@users.noreply.github.com>
2018-07-23 10:17:36 +0700

    Update api

--- routes/web.php
+++ routes/web.php
@@ -85,7 +85,7 @@ Route::get('/console','Client\HomepageController@getListConsole');
 
 
 
-Route::get('category',function (){
+Route::get('/admin/category',function (){
     $category = Category::all();
     return view('admin.listAdmin.SmartPhone.listProductPhone')->with('category',$category);
 });
