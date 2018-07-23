5821a043c3264f11ffb88d01a41f68b61c3549f6
QQAnh <31837158+QQAnh@users.noreply.github.com>
2018-07-23 08:12:37 +0700

    Update api

--- app/Http/Controllers/ProductSmartPhonesController.php
+++ app/Http/Controllers/ProductSmartPhonesController.php
@@ -143,7 +143,7 @@ class ProductSmartPhonesController extends Controller
         $product =Product::where('category','=',3)
             ->get();
 //        return response()->json($product, 200);
-        return view('admin.listAdmin.Product.SmartPhone.listProductPC')->with('product',$product);
+        return view('admin.listAdmin.Product.PC.listProductPC')->with('product',$product);
     }
     public function createPC()
     {
@@ -224,7 +224,7 @@ class ProductSmartPhonesController extends Controller
         $product =Product::where('category','=',2)
             ->get();
 //        return response()->json($product, 200);
-        return view('admin.listAdmin.Product.SmartPhone.listProductLaptop')->with('product',$product);
+        return view('admin.listAdmin.Product.Laptop.listProductLaptop')->with('product',$product);
     }
     public function createLaptop()
     {
@@ -304,7 +304,7 @@ class ProductSmartPhonesController extends Controller
         $product =Product::where('category','=',4)
             ->get();
 //        return response()->json($product, 200);
-        return view('admin.listAdmin.Product.SmartPhone.listProductConsole')->with('product',$product);
+        return view('admin.listAdmin.Product.Console.listProductConsole')->with('product',$product);
     }
     public function createConsole()
     {
