526ffdfb58ddec7f5a8910401a68a6c235a920a7
QQAnh <31837158+QQAnh@users.noreply.github.com>
2018-07-23 15:02:48 +0700

    Update api

--- routes/web.php
+++ routes/web.php
@@ -279,7 +279,7 @@ Route::get('/laptop','Client\HomepageController@getListLaptop');
 Route::get('/pc','Client\HomepageController@getListPC');
 Route::get('/console','Client\HomepageController@getListConsole');
 Route::get('/cart/{id}','Client\HomepageController@getAddCart');
-Route::get('shoppingCart','Client\HomepageController@getShopingCart');
+Route::get('/shoppingCart','Client\HomepageController@getShopingCart');
 
 
 
